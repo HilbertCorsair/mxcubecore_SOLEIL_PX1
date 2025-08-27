3c1bd68f22ca8e63e12cea6f6d6106cbbc0f0367
Dan Costin <dan-tiberiu.costin@synchrotron-soleil.fr>
2025-08-27 14:08:42 +0200

    [FEATURE] First files and architecture for the AutoCollect with automatic sequence handling. Arthur F.

---
+++ mxcubecore/HardwareObjects/SOLEIL/PX1/PX1Unattended.py
@@ -0,0 +1,77 @@
+"""HardwareObject used as wrapper for automatic collects
+
+This is a temporary solution to start and test automatic sequences on MXCuBE Web
+
+"""
+
+import logging
+import gevent
+import time
+import os
+import sys
+
+from mxcubecore import HardwareRepository as HWR
+
+
+log = logging.getLogger("HWR")
+
+class PX1Unattended():
+
+    def __init__(self):
+
+        self.flagZoomValChange = 0
+        pass
+
+    def __getBeamsize__(self):
+        try:
+            size_hor, size_ver = HWR.beamline.beam.get_beam_size()
+            size_hor *= 1000 # Why is it multiplied by 1000
+            size_ver *= 1000 # Why is it multiplied by 1000
+        except Exception:
+            size_hor = None
+            size_ver = None
+        return size_hor, size_ver
+
+    def __bboxToCoor__(self, bbox_loop):
+
+        x1, y1, x2, y2 = -1, -1, -1, -1
+
+        return ((x1, y1), (x2, y2))
+
+    def __getZoomValue__(self):
+        try:
+            currentZoom = HWR.beamline.diffractometer.zoom.get_value()
+        except Exception:
+            currentZoom = -1
+        return currentZoom
+
+    def __setZoomValue__(self, newZoom):
+        try:
+            HWR.beamline.diffractometer.zoom.set_value(newZoom) # Never tested
+            self.flagZoomValChange = newZoom
+        except Exception:
+            self.flagZoomValChange = -1
+
+
+    def StartAutoSequence():
+        """Main method to call to start the collect of current samples in Queue
+
+        Args:
+
+        Returns:
+
+        Raises:
+
+        """
+
+        # For sample in Queue:
+        #   Zoom(1)                                Done
+        #   Mount(sample)
+        #   Murko()
+        #   Zoom(3)                                Done
+        #   bbox_loop = Murko()
+        #   coord = __bboxToCoor__(bbox_loop)      Definition
+        #   hotPoint = XrayCentring(coord)
+        #   Collect(hotPoint)
+
+        pass
